127b1a302c986930cc5cb0022574a528bf97bf8d
MicahMcConahay <micahmcconahay@cedarville.edu>
2025-04-04 11:49:11 -0400

    Update Wire.cpp

--- Wire.cpp
+++ Wire.cpp
@@ -1,2 +1,14 @@
-
-
+#include "Wire.h"
+Wire::Wire() {
+	
+}
+void Wire::setValue() {
+	
+}
+void Wire::setDrives() {}
+void Wire::setHistory() {}
+int Wire::getValue() {
+	return iValue;
+}
+int Wire::getDrives() {}
+int Wire::getIndex() {}
