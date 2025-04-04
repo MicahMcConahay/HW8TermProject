22bfd474f2aa13169760303c3a8490adf145904f
MicahMcConahay <micahmcconahay@cedarville.edu>
2025-04-04 11:48:54 -0400

    Update Wire.h

--- Wire.h
+++ Wire.h
@@ -1,6 +1,25 @@
 #pragma once
-
+#include <string>
+#include <vector>
+using namespace std;
 
 class Gate;
 
-class Wire {};
+class Wire {
+public:
+	Wire();
+	void setValue();
+	void setDrives();
+	void setHistory();
+	int getValue();
+	int getDrives();
+	int getIndex();
+private:
+	int iValue;
+	char cValue;
+	enum eValue;
+	string sName;
+	char cName;
+	vector<Gate*> drives;
+	int index;
+};
