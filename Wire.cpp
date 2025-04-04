80f5ede059a1d3889a40b7bdca0af3a43d52fe83
MicahMcConahay <micahmcconahay@cedarville.edu>
2025-04-04 12:15:21 -0400

    Update Wire.cpp

--- Wire.cpp
+++ Wire.cpp
@@ -2,13 +2,29 @@
 Wire::Wire() {
 	
 }
-void Wire::setValue() {
-	
+void Wire::setValue(int x) {
+	if (x == LOW || x == HIGH || x == UNDEFINED) {
+		iValue = x;
+	}
+	else { iValue = UNDEFINED; }
+}
+void Wire::setValue(char x) {
+	if (x == '0' || x == '1') {
+		iValue = x - '0';
+	}
+	else {
+		iValue = UNDEFINED;
+	}
+	cValue = x;
 }
 void Wire::setDrives() {}
 void Wire::setHistory() {}
 int Wire::getValue() {
 	return iValue;
 }
-int Wire::getDrives() {}
-int Wire::getIndex() {}
+vector<Gate*> Wire::getDrives() {
+	return drives;
+}
+int Wire::getIndex() {
+	return index;
+}
